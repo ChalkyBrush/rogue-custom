d3042e396a8090cdbda412a4908fd1b51d506245
rraciop <ryanracioppo@gmail.com>
2014-01-26 19:15:24 -0500

    clean up interface

--- app/assets/javascripts/shoes.js
+++ app/assets/javascripts/shoes.js
@@ -3,3 +3,17 @@ $(window).load(function(){
   $('#dvLoading').fadeOut(1200);
 });
 
+// $( "#select1" ).click(function() {
+//   selection = 1;
+//   $(this).css({backgroundColor:selectcolor}, 200);
+//   $(".customize-elements").fadeOut(800);
+
+//   $(".customize-elements").delay(200).fadeIn(1000).queue(function(n) {
+//         $(".customize-title").html("Customize Color - Heel");
+//         n();
+//   });
+//   $( "#select2" ).css({backgroundColor:stcolor}, 200);
+//   $( "#select3" ).css({backgroundColor:stcolor}, 200);
+//   $( "#select4" ).css({backgroundColor:stcolor}, 200);
+//   $( "#select5" ).css({backgroundColor:stcolor}, 200);
+// });
